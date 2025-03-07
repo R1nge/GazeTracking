29e45425d128aa223c668cd90206bc3bccfafa88
R1nge <r1ngegamedev@gmail.com>
2025-03-07 14:31:45 +0300

    serialize eyes coordinates

--- tracking.py
+++ tracking.py
@@ -2,14 +2,10 @@ import cv2
 import os
 import glob
 import json
-import numpy as np  # Add numpy import
 from gaze_tracking import GazeTracking
-import time
 
-# Initialize GazeTracking
 gaze = GazeTracking()
 
-# Path to the folder containing .png files
 sharedPath = r'C:\Users\R1nge\Documents\TELEGRAM\SHARED'
 statePath = os.path.join(sharedPath, "STATE")
 statePathJson = os.path.join(statePath, "STATE.json")
@@ -21,24 +17,18 @@ state = {
 with open(statePathJson,'w', encoding='utf-8') as f:
     json.dump(state, f,ensure_ascii=False, indent=4)
 
-# Find all .png files in the folder
 png_files = glob.glob(os.path.join(os.path.join(sharedPath, "GAZE"), '*.jpg'))
 
-# Dictionary to store results
 results = []
 
 print(len(png_files))
 
 i = 0
-# Analyze each .png file
 for png_file in png_files:
-    # Read the image
     frame = cv2.imread(png_file)
 
-    # Send this frame to GazeTracking to analyze it
     gaze.refresh(frame)
 
-    # Get gaze direction
     if gaze.is_blinking():
         direction = "Blinking"
     elif gaze.is_right():
@@ -50,21 +40,38 @@ for png_file in png_files:
     else:
         direction = "Unknown"
 
-    # Get pupil coordinates
     left_pupil = gaze.pupil_left_coords()
     right_pupil = gaze.pupil_right_coords()
 
-    # Store the result in the dictionary
-    data = {
-        'direction': direction
-    }
-    results.append(data)
-    i += 1
-    print(i)
+    if left_pupil is not None:
+        left_eye_x = left_pupil[0]
+        left_eye_y = left_pupil[1]
+    else:
+        left_eye_x = None
+        left_eye_y = None
+
+    if right_pupil is not None:
+        right_eye_x = right_pupil[0]
+        right_eye_y = right_pupil[1]
+    else:
+        right_eye_x = None
+        right_eye_y = None
+
+    if left_pupil is not None or right_pupil is not None:
+        data = {
+            'direction': direction,
+            'left_eye_x': int(left_eye_x),
+            'left_eye_y': int(left_eye_y),
+            'right_eye_x': int(right_eye_x),
+            'right_eye_y': int(right_eye_y)
+        }
+        results.append(data)
+        print(data)
+        i += 1
+        print(i)
 
 print(len(results))
 
-# Save results to a JSON file
 output_file = os.path.join(sharedPath, 'gaze_results.json')
 with open(output_file, 'w') as json_file:
     json.dump(results, json_file, indent=4)
